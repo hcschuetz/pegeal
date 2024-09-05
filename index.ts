87ca22c6a9924fe52659e5cd703ae84c59871ad0
Heribert Schütz <hs@webxcerpt.com>
2024-09-05 22:31:06 +0200

    sandwich usage examples make some use of the currying

--- index.ts
+++ index.ts
@@ -825,22 +825,23 @@ ${alg.exp(blade)}`);
   const alg = new Algebra(euclidean(coords), ctx, makeLetterNames(coords));
 
   for (const create of [
-    () => [alg.mv("a", {x: "ax", y: "ay"}), alg.mv("b", {x: "bx", y: "by", z: "bz"}), alg.mv("c", {x: "cx", y: "cy"})],
-    () => [alg.mv("a", {x: 1   , y: 1   }), alg.mv("b", {x: 1   , y: 1   , z: 1   }), alg.mv("c", {x: "cx", y: "cy"})],
-    () => [alg.mv("a", {x: 1   , y: 1   }), alg.mv("b", {x: 1   , y: 1   , z: 1   }), alg.mv("c", {x: 1.1 , y: 3.3 })],
+    () => [alg.mv("a", {x: "ax", y: "ay"}), alg.mv("b", {x: "bx", y: "by", z: "bz"})],
+    () => [alg.mv("a", {x: 1   , y: 1   }), alg.mv("b", {x: 1   , y: 1   , z: 1   })],
   ]) {
-    const [a, b, c] = create();
+    const [a, b] = create();
     ctx.emit(`// a: ${a}`);
     ctx.emit(`// b: ${b}`);
-    ctx.emit(`// c: ${c}`);
     const ba = alg.geometricProduct(b, a);
     ctx.emit(`// ba: ${ba}`);
     const rotor = alg.normalize(ba);
     ctx.emit(`// rotor: ${rotor}`);
     ctx.emit(`// rotor~: ${alg.reverse(rotor)}`);
-    ctx.emit(`// c: ${c}`);
-    ctx.emit(`// sandwich: ${alg.sandwich(rotor)(c)}`);
-    ctx.emit(`// sandwich1: ${alg.sandwich1(rotor, c)}`);
+    const sw_rotor = alg.sandwich(rotor);
+    for (const c of [alg.mv("a", {x: "ax", y: "ay"}), alg.mv("a", {x: 1, y: 1})]) {
+      ctx.emit(`// c: ${c}`);
+      ctx.emit(`// sandwich: ${sw_rotor(c)}`);
+      ctx.emit(`// sandwich1: ${alg.sandwich1(rotor, c)}`);
+    }
     // console.log(ctx.text); process.exit();
     const a0 = alg.normalize(a);
     const b0 = alg.normalize(b);
@@ -856,7 +857,7 @@ ${alg.exp(blade)}`);
   // Minimalistic example where the cancelling performed by `alg.sandwich(...)`
   // omits the zero-valued xyz component:
   for (const create of [
-    () => [alg.mv("a", {x: 1, y: 1}), alg.mv("b", {z: 1})],
+    () => [alg.mv("a", {x: Math.SQRT1_2, y: Math.SQRT1_2}), alg.mv("b", {z: 1})],
     () => [alg.mv("a", {x: "ax", y: "ay"}), alg.mv("b", {z: "bz"})],
   ]) {
     let [a, b] = create();
@@ -864,7 +865,9 @@ ${alg.exp(blade)}`);
     b = alg.normalize(b);
     ctx.emit(`// a: ${a})`);
     ctx.emit(`// b: ${b})`);
-    ctx.emit(`// sandwich: ${alg.sandwich(a)(b)})`);
+    const sw_a = alg.sandwich(a);
+    ctx.emit(`// sandwich: ${sw_a(b)})`);
+    ctx.emit(`// sandwich: ${sw_a(alg.negate(b))})`);
     ctx.emit(`// sandwich1: ${alg.sandwich1(a, b)})`);
     ctx.emit("---------------------");
   }
