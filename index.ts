71c654ac7825e4f270328dd7384ae544e3ce9ebc
Heribert Schütz <hs@webxcerpt.com>
2024-09-06 23:39:59 +0200

    refined tests for "inverse"

--- index.ts
+++ index.ts
@@ -907,9 +907,9 @@ ${alg.exp(blade)}`);
 // Inverse Check
 `);
 
-  const ctx = new WebGLContext();
+  const ctx = new EvalContext();
   const coords = "xyzw";
-  const alg = new Algebra([1,1,1,-1], ctx, makeLetterNames(coords));
+  const alg = new Algebra([1,3,1,-1], ctx, makeLetterNames(coords));
 
   for (const v of [
     alg.mv("scalar", {1: 7}),
@@ -922,13 +922,26 @@ ${alg.exp(blade)}`);
     alg.mv("f", {xw: 7}),
     alg.mv("g", {xw: 7, yw: 3}),
     alg.mv("h", {1: 4, xy: 7, xz: 3, yz: 2}),
+    alg.geometricProduct(
+      alg.mv("h", {1: 4, xy: 7, xz: 3, yz: 2}),
+      alg.mv("h", {1: 4, xy: 7, xz: 3, yz: 2}),
+    ),
+    alg.geometricProduct(
+      alg.mv("c", {x: 3, y: 4}),
+      alg.mv("h", {1: 4, xy: 7, xz: 3, yz: 2}),
+    ),
   ]) {
-    ctx.emit(`// ---------------`);
-    ctx.emit(`// v: ${v}`);
-    ctx.emit(`// inv(v): ${alg.inverse(v)}`);
-    ctx.emit(`// inv(v)*v: ${alg.geometricProduct(alg.inverse(v), v)}`);
-    ctx.emit(`// v*inv(v): ${alg.geometricProduct(v, alg.inverse(v))}`);
+    p(`---------------`);
+    p(`v: ${v}`);
+    p(`inv(v): ${alg.inverse(v)}`);
+    for (const [label, prod] of [
+      ["inv(v)*v", alg.geometricProduct(alg.inverse(v), v)],
+      ["v*inv(v)", alg.geometricProduct(v, alg.inverse(v))],
+    ] as [string, MultiVector<never>][]) {
+      p(`${label}: ${prod}`);
+      if (alg.normSquared(alg.plus(prod, alg.negate(alg.one()))) > 1e-30) {
+        throw "testing 'inverse' failed";
+      }
+    }
   }
-
-  console.log(ctx.text);
 }
