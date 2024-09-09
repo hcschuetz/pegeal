4f1b576b2d0b1f6bab3482728e5e21aa486864ca
Heribert Schütz <hs@webxcerpt.com>
2024-09-10 01:25:33 +0200

    some renamings within WASMBackEnd

--- src/WASMBackEnd.ts
+++ src/WASMBackEnd.ts
@@ -1,42 +1,42 @@
 import B from "binaryen";
 import { Term, Scalar, BackEnd, Var, truth } from "./Algebra";
 
-export class VarRef implements VarRef {
-  constructor(readonly varNum: number) {}
+export class LocalRef {
+  constructor(readonly locNum: number) {}
 }
 
-class VarImpl extends Var<VarRef> {
+class VarImpl extends Var<LocalRef> {
   constructor(
     readonly be: WASMBackEnd,
   ) {
     super();
   }
 
-  #varRef?: VarRef;
+  #localRef?: LocalRef;
 
-  addTerm(term: Term<VarRef>, negate: truth, create: boolean) {
+  addTerm(term: Term<LocalRef>, negate: truth, create: boolean) {
     const {mod, body, convertFactor} = this.be;
     const expr =
       term.length === 0 ? mod.f64.const(1) :
       term.map(convertFactor).reduce((acc, factor) => mod.f64.mul(acc, factor));
     const signedExpr = negate ? mod.f64.neg(expr) : expr;
     if (create) {
-      this.#varRef = this.be.newLocal();
-      body.push(mod.local.set(this.#varRef.varNum, signedExpr));
+      this.#localRef = this.be.newLocal();
+      body.push(mod.local.set(this.#localRef.locNum, signedExpr));
     } else {
-      body.push(mod.local.set(this.#varRef!.varNum,
-        mod.f64.add(mod.local.get(this.#varRef!.varNum, B.f64), signedExpr)
+      body.push(mod.local.set(this.#localRef!.locNum,
+        mod.f64.add(mod.local.get(this.#localRef!.locNum, B.f64), signedExpr)
       ));
     }
   }
 
-  getValue() { return this.#varRef! };
+  getValue() { return this.#localRef! };
 }
 
-export default class WASMBackEnd extends BackEnd<VarRef> {
+export default class WASMBackEnd extends BackEnd<LocalRef> {
   varCount = 0;
   body: B.ExpressionRef[] = [];
-  paramsByHint: Record<string, VarRef> = {};
+  paramsByHint: Record<string, LocalRef> = {};
 
   constructor(
     readonly mod: B.Module,
@@ -55,25 +55,25 @@ export default class WASMBackEnd extends BackEnd<VarRef> {
   }
 
   newLocal() {
-    return new VarRef(this.varCount++);
+    return new LocalRef(this.varCount++);
   }
 
   /**
    * `convertFactor` is a bound function so that it can be used as
    * `[...].map(this.convertFactor)`
    */
-  convertFactor = (f: Scalar<VarRef>) => {
+  convertFactor = (f: Scalar<LocalRef>): B.ExpressionRef => {
     switch (typeof f) {
       case "number": return this.mod.f64.const(f);
-      case "object": return this.mod.local.get(f.varNum, B.f64);
+      case "object": return this.mod.local.get(f.locNum, B.f64);
     }
   }
 
-  scalarOp(name: string, ...args: Scalar<VarRef>[]) {
+  scalarOp(name: string, ...args: Scalar<LocalRef>[]) {
     const {mod, convertFactor} = this;
     const localVar = this.newLocal();
     this.body.push(
-      mod.local.set(localVar.varNum,
+      mod.local.set(localVar.locNum,
         Object.hasOwn(binopName, name)
         ? mod.f64[binopName[name]](convertFactor(args[0]), convertFactor(args[1]))
         : mod.call(name, args.map(convertFactor), B.f64)
